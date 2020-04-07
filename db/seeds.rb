87d651f835fb07040e1466d939abf28459bd9e31
Peter Jang <peter.x.jang@gmail.com>
2020-04-07 09:40:32 -0500

    Initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,2 @@
+Contact.create!(first_name: "Saron", last_name: "Yitbarek", email: "saron@example.com", phone_number: "3243243244")
+Contact.create!(first_name: "Majora", last_name: "Carter", email: "majora@example.com", phone_number: "3243243245")
